21673bddd844c873947e209932b68b5ff2459b9f
mpacala00 <mpacala00@gmail.com>
2023-07-25 20:43:49 +0200

    Basics of EntityManager

--- src/main/java/com/github/mpacala00/Main.java
+++ src/main/java/com/github/mpacala00/Main.java
@@ -1,7 +1,29 @@
 package com.github.mpacala00;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // obtaining EntityManagerFactory while using persistence.xml for config
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        // through entityManager you operate on Hibernate context
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        try {
+            //all changes to entities will be synchronized with database after getTransaction().commit()
+            entityManager.getTransaction().begin();
+            //put operations on entities here:
+
+            entityManager.getTransaction().commit();
+        } finally {
+            // clear persistence context; detach all entities from the context
+            entityManager.clear();
+        }
+
+
     }
 }
