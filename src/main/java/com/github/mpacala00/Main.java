c6e0a5485fdf1cb71cb46b9d789e0dff4d3e27ab
mpacala00 <mpacala00@gmail.com>
2023-07-26 17:55:24 +0200

    Programmatic approach to configuring persistence unit

--- src/main/java/com/github/mpacala00/Main.java
+++ src/main/java/com/github/mpacala00/Main.java
@@ -1,15 +1,22 @@
 package com.github.mpacala00;
 
 import com.github.mpacala00.entity.Product;
+import com.github.mpacala00.persistence.PersistenceUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceUnit;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
         // obtaining EntityManagerFactory while using persistence.xml for config
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+        //EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManagerFactory entityManagerFactory = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new PersistenceUnitInfo(), new HashMap<>()); // empty hashmap -> no parameters
 
         // through entityManager you operate on Hibernate context
         EntityManager entityManager = entityManagerFactory.createEntityManager();
@@ -19,8 +26,10 @@ public class Main {
             entityManager.getTransaction().begin();
             //put operations on entities here:
             Product product = new Product();
-            product.setId(1L);
-            product.setName("Some product");
+//            product.setId(1L);
+//            product.setName("Some product");
+            product.setId(2L);
+            product.setName("Other product");
 
             // persist() -> add this to the context
             // NOT AN INSERT
---
+++ src/main/java/com/github/mpacala00/persistence/PersistenceUnitInfo.java
@@ -0,0 +1,105 @@
+package com.github.mpacala00.persistence;
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.SharedCacheMode;
+import jakarta.persistence.ValidationMode;
+import jakarta.persistence.spi.ClassTransformer;
+import jakarta.persistence.spi.PersistenceUnitTransactionType;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+// a class defining everything that can be put inside persistence.xml
+public class PersistenceUnitInfo implements jakarta.persistence.spi.PersistenceUnitInfo {
+    @Override
+    public String getPersistenceUnitName() {
+        return "my-persistence-unit";
+    }
+
+    @Override
+    public String getPersistenceProviderClassName() {
+        return "org.hibernate.jpa.HibernatePersistenceProvider";
+    }
+
+    @Override
+    public PersistenceUnitTransactionType getTransactionType() {
+        return PersistenceUnitTransactionType.RESOURCE_LOCAL;
+    }
+
+    @Override
+    public DataSource getJtaDataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/jpa_hibernate_overview");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("password12");
+
+        return dataSource;
+    }
+
+    @Override
+    public DataSource getNonJtaDataSource() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMappingFileNames() {
+        return null;
+    }
+
+    @Override
+    public List<URL> getJarFileUrls() {
+        return null;
+    }
+
+    @Override
+    public URL getPersistenceUnitRootUrl() {
+        return null;
+    }
+
+    @Override
+    public List<String> getManagedClassNames() {
+        return List.of("com.github.mpacala00.entity.Product");
+    }
+
+    @Override
+    public boolean excludeUnlistedClasses() {
+        return false;
+    }
+
+    @Override
+    public SharedCacheMode getSharedCacheMode() {
+        return null;
+    }
+
+    @Override
+    public ValidationMode getValidationMode() {
+        return null;
+    }
+
+    @Override
+    public Properties getProperties() {
+        return null;
+    }
+
+    @Override
+    public String getPersistenceXMLSchemaVersion() {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return null;
+    }
+
+    @Override
+    public void addTransformer(ClassTransformer classTransformer) {
+
+    }
+
+    @Override
+    public ClassLoader getNewTempClassLoader() {
+        return null;
+    }
+}
